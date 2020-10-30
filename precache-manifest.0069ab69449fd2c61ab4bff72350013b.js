b76c4b1dee4af28525eaca96a33b4457699fd647
Sintayehu <sintemite@gmail.com>
2020-10-30 00:25:27 -0400

    Updates

---
+++ precache-manifest.0069ab69449fd2c61ab4bff72350013b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4fecdfdc9abb763af86b16dab203630",
+    "url": "/index.html"
+  },
+  {
+    "revision": "059bd623ce8417e99a33",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "8cebbafc756e75891e81",
+    "url": "/static/css/main.5fde287f.chunk.css"
+  },
+  {
+    "revision": "059bd623ce8417e99a33",
+    "url": "/static/js/2.b60c9ab6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.b60c9ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cebbafc756e75891e81",
+    "url": "/static/js/main.ff55535d.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "f8f7e166b12eecd762970a876e6f4557",
+    "url": "/static/media/headshot.f8f7e166.jpg"
+  }
+]);
